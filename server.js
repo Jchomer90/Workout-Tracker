13c64183d0cbe04345ee2bbb29f9ceb3d8d3434b
Josh Homer <joshuachomer@gmail.com>
2020-07-28 23:04:46 -0400

    Updated routes, not full functional post/put

--- server.js
+++ server.js
@@ -22,9 +22,73 @@ app.get("/stats", (req, res) => {
     res.sendFile(__dirname + "/public/stats.html")
 });
 
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).populate("exercises")
+
+    .then(workout => {
+        // psuedo code
+        console.log(workout);
+        console.log(res);
+
+        let newWorkoutArray = [];
+        for(let i = 0; i < workout.length; i++) {
+            let newWorkoutObject;
+            // let duration;
+            for (let j = 0; workout[i].exercises.length; j++) {
+                totalDuration += workout[i].exercises[j].duration;
+            }
+            newWorkoutObject = { day: workout[i].day, exercise: workout[i].exercises, duration: duration }
+
+            newWorkoutArray.push(newWorkoutObject);
+        }
+        res.json(workout);
+
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Exercise.create(req.body) 
+        .then((exerciseData) => {
+            db.Workout.findOneAndUpdate({ _id: req.params.id }, {exercises: exerciseData._id }, { new: true })
+                .then(data => {
+                    res.json(data);
+                })
+                    .catch(err => {
+                        res.json(err);
+                    });
+
+            // res.json(exerciseData)
+        });
+});
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        })
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7).populate("exercises")
+        .then(data => {
+            res.json(data);
+            console.log(data);
+        })
+            .catch(err => {
+                res.json(err);
+                console.log(err);
+            })
+});
 
 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`The server is listening on port ${PORT}...`);
-})
+});
